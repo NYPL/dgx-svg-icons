44764c1836106e84224f3e22a128d3f5d42c4fe3
Rafael Hernandez <rafaelhernandez@nypl.org>
2016-05-04 12:20:29 -0400

    Added dots icon for Blog use

--- src/icons.jsx
+++ src/icons.jsx
@@ -1,9 +1,11 @@
 import LionLogoIcon from './components/LionLogoIcon.jsx';
 import CarouselCircleIcon from './components/CarouselCircleIcon.jsx';
 import BookIcon from './components/BookIcon.jsx';
+import DotsIcon from './components/DotsIcon.jsx';
 
 export {
   LionLogoIcon,
   CarouselCircleIcon,
   BookIcon,
+  DotsIcon,
 };
