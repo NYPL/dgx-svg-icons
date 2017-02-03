92ad7487b88cb66bb33c980b7b5aec70c24fd665
ricardo galvez <ricardogalvez@nypl.org>
2017-02-03 15:11:28 -0500

    updated the app so that the title id is being picked up ty the aria-labelledby attribute on the parent svg element. seems like this is the right approach and passes wave, axe & chrome accessibility audits, unfortunately the way the current page is constructed its difficult to get VoiceOver / tabbing to work right

--- src/components/LoginIcon.jsx
+++ src/components/LoginIcon.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-const LoginIcon = ({ viewBox, height, width, title, className, style, fill, ariaHidden, preserveAspectRatio, ariaDesc, }) => (
+const LoginIcon = ({ viewBox, height, width, title, className, style, fill, ariaHidden, preserveAspectRatio, ariaDesc, iconId, svgRole, }) => (
   <svg
     viewBox={viewBox}
     height={height}
@@ -9,8 +9,10 @@ const LoginIcon = ({ viewBox, height, width, title, className, style, fill, aria
     fill={fill}
     aria-hidden={ariaHidden}
     preserveAspectRatio={preserveAspectRatio}
+    aria-labelledby={iconId}
+    role={svgRole}
   >
-  <title aria-label={ariaDesc}>{title}</title>
+  <title id={iconId}>{title}</title>
   <path d="M16.07184,6.52418c1.24313,0,2.29334,1.39823,2.29334,3.05329s-1.05022,3.05329-2.29334,3.05329S13.7785,11.23252,13.7785,9.57746s1.05022-3.05329,2.29334-3.05329m0-1.8c-2.26069,0-4.09334,2.17289-4.09334,4.85329s1.83265,4.85329,4.09334,4.85329,4.09334-2.17289,4.09334-4.85329-1.83265-4.85329-4.09334-4.85329Z" opacity="0.85"/>
   <path d="M25.28125,27.34375H21a1,1,0,0,1,0-2h4.28125l-2.377-6.2041a.28873.28873,0,0,0-.10547-.11719,47.03461,47.03461,0,0,0-6.7832-.585,48.40643,48.40643,0,0,0-6.957.6084h.00049c.0249,0-.00244.03418-.02246.085l-2.355,6.14551L11,25.34375a1,1,0,0,1,0,2H6.71826a2.04254,2.04254,0,0,1-1.90088-2.791l2.355-6.14551a2.07691,2.07691,0,0,1,1.57617-1.33789,50.4409,50.4409,0,0,1,7.26709-.63184,48.54187,48.54187,0,0,1,7.11816.61621,2.19071,2.19071,0,0,1,1.63184,1.35352l2.417,6.14551a2.04377,2.04377,0,0,1-1.90137,2.791Z" opacity="0.85"/>
   </svg>
@@ -27,6 +29,8 @@ LoginIcon.propTypes = {
   ariaHidden: React.PropTypes.bool,
   preserveAspectRatio: React.PropTypes.string,
   ariaDesc: React.PropTypes.string,
+  iconId: React.PropTypes.string,
+  svgRole: React.PropTypes.string,
 };
 
 LoginIcon.defaultProps = {
@@ -36,7 +40,8 @@ LoginIcon.defaultProps = {
   height: '32',
   viewBox: '0 0 32 32',
   preserveAspectRatio: 'xMidYMid meet',
-  ariaDesc: 'NYPL Login Icon',
+  iconId: 'login-icon',
+  svgRole: 'img',
 };
 
 export default LoginIcon;
--- src/components/LoginIconSolid.jsx
+++ src/components/LoginIconSolid.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-const LoginIconSolid = ({ viewBox, height, width, title, className, style, fill, ariaHidden, preserveAspectRatio, ariaDesc, }) => (
+const LoginIconSolid = ({ viewBox, height, width, title, className, style, fill, ariaHidden, preserveAspectRatio, ariaDesc, iconId, svgRole, }) => (
   <svg
     viewBox={viewBox}
     height={height}
@@ -9,8 +9,10 @@ const LoginIconSolid = ({ viewBox, height, width, title, className, style, fill,
     fill={fill}
     aria-hidden={ariaHidden}
     preserveAspectRatio={preserveAspectRatio}
+    aria-labelledby={iconId}
+    role={svgRole}
   >
-  <title aria-label={ariaDesc}>{title}</title>
+  <title id={iconId}>{title}</title>
   <ellipse cx="16.07184" cy="9.57746" rx="4.09334" ry="4.85329"/>
   <path d="M25.25882,27.3451,6.61176,27.33725a2.05352,2.05352,0,0,1-1.87843-2.5451l2.46189-6.50245a2.1203,2.1203,0,0,1,1.34706-1.18431,45.76876,45.76876,0,0,1,7.00567-.657c2.94823,0,5.86851.45206,7.09041.53934a2.20375,2.20375,0,0,1,1.97255,1.1098l2.62353,6.64706A2.01818,2.01818,0,0,1,25.25882,27.3451Z"/>
   </svg>
@@ -28,16 +30,19 @@ LoginIconSolid.propTypes = {
   ariaHidden: React.PropTypes.bool,
   preserveAspectRatio: React.PropTypes.string,
   ariaDesc: React.PropTypes.string,
+  iconId: React.PropTypes.string,
+  svgRole: React.PropTypes.string,
 };
 
 LoginIconSolid.defaultProps = {
   className: 'loginIcon',
-  title: 'Log in to NYPL',
+  title: 'You are logged in',
   width: '32',
   height: '32',
   viewBox: '0 0 32 32',
   preserveAspectRatio: 'xMidYMid meet',
-  ariaDesc: 'NYPL Login Icon',
+  iconId: 'loggedin-icon',
+  svgRole: 'img',
 };
 
 export default LoginIconSolid;
