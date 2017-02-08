2fa2d78f75bd625622e8c7a306f8c55c8eb954e0
ricardo galvez <ricardogalvez@nypl.org>
2017-02-08 16:50:41 -0500

    added the building icon and added role attribute

--- src/components/BuildingIcon.jsx
+++ src/components/BuildingIcon.jsx
@@ -14,12 +14,12 @@ const BuildingIcon = ({ viewBox, height, width, title, className, style, fill, a
     role={svgRole}
   >
   <title id={iconId}>{title}</title>
-      <polygon points="8 10 16 7 24 10 28 10 16 5 4 10 8 10" />
-      <polygon points="28 25 26 25 26 16 27 16 27 14 24 14 24 16 24 25 8 25 8 16 8 14 5 14 5 16 6 16 6 25 4 25 4 27 28 27 28 25" />
-      <rect x="5" y="11" width="22" height="2" />
-      <rect x="10" y="15" width="2" height="9" />
-      <rect x="20" y="15" width="2" height="9" />
-      <rect x="15" y="15" width="2" height="9" />
+    <polygon points="11.333 10 16 7.024 20.667 10 23 10 16 5.042 9 10 11.333 10" />
+    <polygon points="28 25 26 25 26 16 27 16 27 14 24 14 24 16 24 25 8 25 8 16 8 14 5 14 5 16 6 16 6 25 4 25 4 27 28 27 28 25" />
+    <rect x="5" y="11" width="22" height="2" />
+    <rect x="10" y="15" width="2" height="9" />
+    <rect x="20" y="15" width="2" height="9" />
+    <rect x="15" y="15" width="2" height="9" />
     </svg>
 );
 
@@ -43,6 +43,7 @@ BuildingIcon.defaultProps = {
   height: '32',
   viewBox: '0 0 32 32',
   preserveAspectRatio: 'xMidYMid meet',
+  svgRole: 'img',
 };
 
 export default BuildingIcon;
