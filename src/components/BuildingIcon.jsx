1dc60e54d0d219d9d6851c600116bf7765aca1d1
ricardo galvez <ricardogalvez@nypl.org>
2017-02-08 17:58:31 -0500

    updated the icon design, hoping for better readability at small scales

--- src/components/BuildingIcon.jsx
+++ src/components/BuildingIcon.jsx
@@ -17,9 +17,9 @@ const BuildingIcon = ({ viewBox, height, width, title, className, style, fill, a
     <polygon points="11.333 10 16 7.024 20.667 10 23 10 16 5.042 9 10 11.333 10" />
     <polygon points="28 25 26 25 26 16 27 16 27 14 24 14 24 16 24 25 8 25 8 16 8 14 5 14 5 16 6 16 6 25 4 25 4 27 28 27 28 25" />
     <rect x="5" y="11" width="22" height="2" />
-    <rect x="10" y="15" width="2" height="9" />
-    <rect x="20" y="15" width="2" height="9" />
-    <rect x="15" y="15" width="2" height="9" />
+    <rect x="10" y="15" width="2" height="8" />
+    <rect x="20" y="15" width="2" height="8" />
+    <rect x="15" y="15" width="2" height="8" />
     </svg>
 );
 
