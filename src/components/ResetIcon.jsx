afd3140b4a87a5b207a57c300b1b3b4d12108fde
Kang Peng <kangtingpeng@nypl.org>
2017-10-06 17:15:27 -0400

    Updated attributes.

--- src/components/ResetIcon.jsx
+++ src/components/ResetIcon.jsx
@@ -9,6 +9,7 @@ const ResetIcon = ({ viewBox, height, width, title, className, fill, ariaHidden 
     className={`${className} svgIcon`}
     aria-hidden={ariaHidden}
     aria-labelledby={title}
+    fill={fill}
   >
     <title id={title}>{title}</title>
     <path d="M10.96075,11l4.60907-3.19434a1,1,0,0,0-1.13965-1.64355L5.939,12.04688l8.83594,6.248a0.99981,0.99981,0,0,0,1.1543-1.63281L10.75061,13H23v8H6a1,1,0,0,0,0,2H25V11H10.96075Z" />
