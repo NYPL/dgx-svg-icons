cb833c15060aaaede80079e0f3c9a1f21fb16942
Edwin Guzman <edwin.gzmn@gmail.com>
2017-11-16 11:55:13 -0500

    Removing older heroku testing attempts and reverting server.js

--- server.js
+++ server.js
@@ -1,40 +1,18 @@
-var webpack = require('webpack'),
-  WebpackDevServer = require('webpack-dev-server'),
-  config = require('./webpack.config'),
-  pkg = require('./package.json');
+var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+var config = require('./webpack.config');
+var pkg = require('./package.json');
 
-if (process.env.NODE_ENV === 'development') {
-  new WebpackDevServer(webpack(config), {
-    publicPath: config.output.publicPath,
-    hot: true,
-    stats: false,
-    inline: true,
-    historyApiFallback: true
-  }).listen(3000, 'localhost', function (err, result) {
-    if (err) {
-      console.log(err);
-    }
-    console.log('Currently building: ' + pkg.name);
-    console.log('Webpack Development Server listening at localhost:3000');
-  });
-} else {
-  var express = require('express');
-  var path = require('path');
-  var ROOT_PATH = __dirname;
-  var INDEX_PATH = path.resolve(ROOT_PATH, 'styleguide');
-
-  var app = express();
-
-  app.set('port', process.env.PORT || 3001);
-  app.use(express.static(INDEX_PATH));
-  // app.get('/', function(req, res) {
-  //   res.status(200).return()
-  // })
-
-  var server = app.listen(app.get('port'), function(error) {
-    if (error) {
-      console.log(error);
-    }
-    console.log('Express server is listening at localhost: 3000');
-  });
-}
+new WebpackDevServer(webpack(config), {
+  publicPath: config.output.publicPath,
+  hot: true,
+  stats: false,
+  inline: true,
+  historyApiFallback: true
+}).listen(3000, 'localhost', function (err, result) {
+  if (err) {
+    console.log(err);
+  }
+  console.log('Currently building: ' + pkg.name);
+  console.log('Webpack Development Server listening at localhost:3000');
+});
